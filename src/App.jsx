549293b9aad81810ded11f5a0c1fd01ccb7c8161
hasanuidesigner <hasanuidesigner@gmail.comm>
2023-09-18 20:57:54 +0530

    innerpage redirect commit5

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   return (
     <>
        
-       <BrowserRouter>
+       <BrowserRouter basename='/'>
         <Navbar/>
       <Routes>         
         <Route path="/" element={<Landpage/>} />
