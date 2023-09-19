d6acb55597dcd14cfbfd0736b48691e649ab222d
hasanuidesigner <hasanuidesigner@gmail.comm>
2023-09-19 06:09:52 +0530

    innerpage redirect commit7

--- src/topnavbar.jsx
+++ src/topnavbar.jsx
@@ -5,9 +5,9 @@ export default function Navbar(){
 return<>
           <div className="navbar-wrap">
          <NavLink to='/' >Home</NavLink> |
-         <NavLink to='/menulist'>Menulist</NavLink> |
-         <NavLink to='/managemenu'>Manage Menu</NavLink> |
-        <NavLink to='/menuadd'>Menu Add</NavLink>
+         <NavLink to='menulist'>Menulist</NavLink> |
+         <NavLink to='managemenu'>Manage Menu</NavLink> |
+        <NavLink to='menuadd'>Menu Add</NavLink>
         {/* <NavLink to={'/menuupdate/1'}>Menu Update</NavLink> */}
         </div>
 </>
