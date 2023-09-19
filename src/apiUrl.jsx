8c6cc421c2b0075d0cc594a9e5639e23e0eb7c9c
hasanuidesigner <hasanuidesigner@gmail.comm>
2023-09-19 19:43:27 +0530

    newDBuse11

--- src/apiUrl.jsx
+++ src/apiUrl.jsx
@@ -2,7 +2,3 @@
 //export const apilink = 'http://localhost:8000/hotelmenu/';
  export const apilink = 'https://my-json-server.typicode.com/hasanuidesigner/hotelmenu/db/';
 //export const apilink = 'https://my-json-server.typicode.com/hasanuidesigner/hotelmenu/hotelmenu/';
-
-
-
-
