38c02ba5cc2f3a769bfe1b702d6c13b527c1d993
Hasan <hasanuidesigner@gmail.com>
2023-09-20 11:33:40 +0530

    jsonScriptAdded8

--- src/apiUrl.jsx
+++ src/apiUrl.jsx
@@ -1,5 +1,4 @@
  
-//export const apilink = 'http://localhost:8000/hotelmenu/';
+export const apilink = 'http://localhost:8000/hotelmenu/';
 // export const apilink = 'https://my-json-server.typicode.com/hasanuidesigner/hotelmenu/db/';
- //export const apilink = 'https://my-json-server.typicode.com/hasanuidesigner/hotelmenu/hotelmenu/';
- export const apilink = 'http://localhost:8001/hotelmenu/';
+ //export const apilink = 'https://my-json-server.typicode.com/hasanuidesigner/hotelmenu/hotelmenu/'; 
