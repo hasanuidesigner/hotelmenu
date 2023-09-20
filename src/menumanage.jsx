83e49a1b8534c234ada3af5fea2b5e2b5c481348
Hasan <hasanuidesigner@gmail.com>
2023-09-20 17:05:26 +0530

    fix deleteitems

--- src/menumanage.jsx
+++ src/menumanage.jsx
@@ -4,69 +4,69 @@ import { EyeFill, PencilSquare, Trash2Fill } from "react-bootstrap-icons"
 import { useNavigate } from "react-router-dom"
 import axios from 'axios';
 
-export default function Menumanage(){
-     
-    const [menumanage,setMenumanage] = useState([]);
-    const navigate = useNavigate();
-    const getMenu = async ()=>{
-      const menu =await axios.get(apilink);
-      setMenumanage(menu.data);
-    }
+export default function Menumanage() {
 
-    const removeitem = async (id) =>{
-        let deleteconfirm = window.confirm('Are you sure to delete the item ID number ' + id + ' in menu?');
-        if (deleteconfirm)
-        {
-       await axios.delete(apilink+id);
-       navigate('/managemenu');
-       getMenu();
-    }
-    }
+  const [menumanage, setMenumanage] = useState([]);
+  const navigate = useNavigate();
+  const getMenu = async () => {
+    const menu = await axios.get(apilink);
+    setMenumanage(menu.data);
+  }
 
-    const viewitem = (id) =>{
-      navigate('/viewmenu/'+id);
+  const removeitem = async (id) => {
+    const delitm = menumanage.map(x => x).filter(y => y.id == id); 
+    let deleteconfirm = window.confirm('Are you sure to remove "' + delitm[0].menuitemname + '" from menu?');
+    if (deleteconfirm) {
+      await axios.delete(apilink + '/' + id);
+      navigate('/managemenu');
+      getMenu();
     }
+  }
 
-    const edititem = (id)=>{
-      navigate('/menuupdate/'+id);
-    }
+  const viewitem = (id) => {
+    navigate('/viewmenu/' + id);
+  }
 
-    useEffect(()=>{
-        getMenu();
-    },[])
+  const edititem = (id) => {
+    navigate('/menuupdate/' + id);
+  }
 
-    return<>
-<div className="content-wrap form-cover-out tbl-list-wrap">
-        <h1 className="inner-page-head">Manage menu items</h1>
-<table className="table table-striped table-bordered menu-table">
-    <thead>
-      <tr>
-      <th>Menu Id</th>
-        <th>Menu Name</th>
-        <th>Menu Description</th>
-        <th>Menu Price (in Rs.)</th>
-        <th colSpan={2}>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-        {
-    menumanage.map((itm)=>
-      <tr key={itm.id}>
-        <td>{itm.id}</td>
-        <td>{itm.menuitemname}</td>
-        <td> {itm.menuitemdesc}</td>
-        <td>{itm.menuitemprice}</td>
-        <td colSpan={2}>
-           
-              <span  className="view-item"  title="View item" onClick={()=>viewitem(itm.id)}><EyeFill/> </span>
-              <span  className="edit-item"  title="Edit item" onClick={()=>edititem(itm.id)}><PencilSquare/></span>
-              <span  className="remove-item"  title="Remove item" onClick={()=>removeitem(itm.id)}><Trash2Fill/></span>
-              
-</td>        
-      </tr>    
-      )}   
-    </tbody>
-  </table>
-  </div>
-    </>
+  useEffect(() => {
+    getMenu();
+  }, [])
+
+  return <>
+    <div className="content-wrap form-cover-out tbl-list-wrap">
+      <h1 className="inner-page-head">Manage menu items</h1>
+      <table className="table table-striped table-bordered menu-table">
+        <thead>
+          <tr>
+            <th>Menu Id</th>
+            <th>Menu Name</th>
+            <th>Menu Description</th>
+            <th>Menu Price (in Rs.)</th>
+            <th colSpan={2}>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            menumanage.map((itm) =>
+              <tr key={itm.id}>
+                <td>{itm.id}</td>
+                <td>{itm.menuitemname}</td>
+                <td> {itm.menuitemdesc}</td>
+                <td>{itm.menuitemprice}</td>
+                <td colSpan={2}>
+
+                  <span className="view-item" title="View item" onClick={() => viewitem(itm.id)}><EyeFill /> </span>
+                  <span className="edit-item" title="Edit item" onClick={() => edititem(itm.id)}><PencilSquare /></span>
+                  <span className="remove-item" title="Remove item" onClick={() => removeitem(itm.id)}><Trash2Fill /></span>
+
+                </td>
+              </tr>
+            )}
+        </tbody>
+      </table>
+    </div>
+  </>
 }
